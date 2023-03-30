0ec80654bc7ffa5b9eb7bc11a4a0624aad36d436
Behrad Koohy <bk2g18@soton.ac.uk>
2023-03-30 14:26:22 +0100

    added model, doing some stuff

---
+++ model.py
@@ -0,0 +1,36 @@
+import torch
+import numpy as np
+
+# Load the data from the file
+data = np.loadtxt('cancer_data.txt', delimiter=',', skiprows=1)
+
+# Split the data into features and attributes
+features = torch.from_numpy(data[:, :6])
+attributes = torch.from_numpy(data[:, 6:])
+
+# Define the linear neural network
+model = torch.nn.Sequential(
+    torch.nn.Linear(6, 1)
+)
+
+# Define the loss function and optimizer
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+# Train the model
+for epoch in range(100):
+    # Forward pass
+    outputs = model(features.float())
+    loss = criterion(outputs, attributes.float())
+    
+    # Backward and optimize
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    # Print progress
+    if (epoch+1) % 10 == 0:
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, 100, loss.item()))
+
+# Save the trained model
+torch.save(model.state_dict(), 'linear_nn.pth')
